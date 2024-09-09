36d041fa0e21b3b503f08d27149b0b2a59aaa7c3
Riya Das <168464803+RDas2000@users.noreply.github.com>
2024-09-09 12:30:32 +0530

    Update M_SecondPageChild.jsx

--- src/components/Movie/M_SecondPageChild.jsx
+++ src/components/Movie/M_SecondPageChild.jsx
@@ -6,7 +6,7 @@ export default function M_SecondPageChild({info,onClose}) {
    console.log(info.movieInfo.Title);
   return (
     <div className=' fixed inset-0 bg-black bg-opacity-30 backdrop-blur-sm text-white flex justify-center items-center '>
-        <div className='mt-14 flex-col gap-5 text-white place-self-center bg-zinc-800 bg-opacity-70'>
+        <div className='sc-bg mt-14 flex-col gap-5 text-white place-self-center bg-zinc-800 bg-opacity-70'>
             <button onClick={onClose} className='place-self-end float-end h-8 w-8 pl-2 hover:bg-zinc-400'><RxCross2/></button>
             <div className='sc-total-div mt-10 flex gap-10 m-3'>
                 <div className='sc-image h-full w-72'><img src={info.movieInfo.Poster} alt={info.movieInfo.Title}/></div>
